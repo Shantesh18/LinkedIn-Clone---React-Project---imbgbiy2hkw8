466a1dd1a4137f477e4dc1c3122f34c3c77f2842
Shantesh18 <113466082+Shantesh18@users.noreply.github.com>
2023-01-18 19:37:40 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import '../styles/App.css';
+import Header from './Header/Header';
+import Main from './Main/Main'
+import Footer from './Footer/Footer';
 const App = () => {
 
 
   return (
     <div id="main">
+      <Header/>
+      <Main/>
+      <Footer/>
+      <h1>Hello LinkedIn Clone</h1>
     </div>
   )
 }
